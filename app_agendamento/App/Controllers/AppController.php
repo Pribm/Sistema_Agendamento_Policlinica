11a0aea63301c99ec4ae4b806b9b25518f89b967
Paulo Monteiro <monteiro.paulovinicius@gmail.com>
2021-01-21 02:36:43 -0300

    limite de atendimentos implementado
    Implementado um limite de atendimento po data e horário, baseados pelo cadastro do médico no banco de dados.

--- app_agendamento/App/Controllers/AppController.php
+++ app_agendamento/App/Controllers/AppController.php
@@ -1,459 +1,189 @@
 <?php
-
 namespace App\Controllers;
-
-
-
 use MF\Model\Container;
-
 use MF\Controller\Action;
-
 use MF\Controller\FormInputFunctions;
-
-
-
-
-
 class AppController extends Action{
-
-
-
-
-
     //as funções dessa classe equivalem as actions estabelecidas dentro da rota
-
     public function navAgendaPaciente(){
-
         $this->validaAutenticacao();
-
         $this->view->status = '';
-
-
-
         $medicos = Container::getModel('Usuario');
-
-        $this->view->medicos = $medicos->listaMedicos();
-
-
-
         $this->render('agenda_paciente', 'layout');
-
     }
-
-
 
     public function navAgendaDia(){
-
         $this->validaAutenticacao();
-
-
-
         $usuario = Container::getModel('Usuario');
-
         $this->view->medicos = $usuario->listaMedicosAtivosInativos();
-
-
-
         $this->view->agendamentos = $usuario->listaAgendamentos();
-
-
-
         $this->render('agenda_dia', 'layout');
-
     }
-
-
 
     public function navCadastraFuncionario(){
-
         $this->validaAutenticacao();
-
-
-
         $setores = Container::getModel('Usuario');
-
-
-
         //incluir no formulario da view os setores.
-
         $this->view->setores = $setores->listaSetores();
-
-
-
+        $this->view->horarios = $setores->listaHorarios();
         //incluir na tabela os funcionarios cadastrados.
-
         $this->view->funcionarios = $setores->listaFuncionarios();
-
-
-
         $this->render('cadastra_funcionario', 'layout');
-
     }
-
-
 
     public function navRelatorio_atendidos(){
-
         $this->validaAutenticacao();
-
-
-
         $medicos = Container::getModel('Usuario');
-
         $this->view->medicos = $medicos->listaMedicosAtivosInativos();
-
-        
-
         $this->render('relatorio_atendidos', 'layout');
-
     }
-
-
-
-
 
     //*********---------|| FIM DOS NAVS ||---------------********//
-
     public function cadastraFuncionario(){
-
         $this->validaAutenticacao();
-
-
-
         $response['email'] = $_POST['email'];
-
         $response['senha'] = $_POST['senha'];
-
         $response['nome'] = $_POST['nome'];
-
         $response['telefone'] = $_POST['telefone'];
-
         $response['id_setor'] = $_POST['id_setor'];
-
-        
-
         $funcionario = Container::getModel('Usuario');
-
-
-
         if($_POST['email'] != '' && $_POST['senha'] != '' && $_POST['nome'] != '' && $_POST['telefone'] != '' && $_POST['id_setor'] != ''){
-
+            /*echo '<pre>';
+            print_r($_POST);
+            echo '</pre>';*/
             $funcionario->__set('email', $_POST['email']);
-
             $funcionario->__set('senha', $_POST['senha']);
-
             $funcionario->__set('nome', $_POST['nome']);
-
             $funcionario->__set('telefone', $_POST['telefone']);
-
             $funcionario->__set('id_setor', $_POST['id_setor']);
-
-        
-
+            $funcionario->__set('id_horario', $_POST['id_horario']);
             $funcionario->cadastraFuncionario();
-
         }
-
-        
-
-
-
         $this->view->setores = $funcionario->listaSetores();
-
-        
-
+        $this->view->horarios = $funcionario->listaHorarios();
         echo json_encode($funcionario->listaFuncionarios());
-
     }
 
+    public function insereHorarioAtendimentos(){
 
+    }
 
     public function listaFuncionarios(){
-
         $funcionario = Container::getModel('Usuario');
-
-        $response = $funcionario->listaFuncionarios();
-
-        /*echo '<pre>';
-
-        print_r($response);
-
-        echo '</pre>';*/
-
-        //$r = utf8_encode(print_r($response));
-
-        echo json_encode($response, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
-
+        $response['todos_funcionarios'] = $funcionario->listaFuncionarios();
+        $response['medicos'] = $funcionario->listaMedicos();
+        echo json_encode($response);
     }
-
-
 
     public function fazerUpload(){
-
         $formatos_permitidos = array('gif', 'jpg', 'jpeg', 'png');
-
         $extensao = pathinfo($_FILES['arquivo']['name'], PATHINFO_EXTENSION );
-
-    
-
         if(in_array($extensao, $formatos_permitidos)){
-
             $pasta = "img/avatar/";
-
             $temporario = $_FILES['arquivo']['tmp_name'];
-
             $novo_nome = uniqid().".$extensao";
-
-
-
             if(move_uploaded_file($temporario, $pasta.$novo_nome)){
-
                 $mensagem = "Upload Feito com sucesso";
-
             }else{
-
                 $mensagem = "Erro! Não foi possível realizar o upload";  
-
             }
-
-
-
             echo $mensagem;
-
         }else{
-
             $mensagem = "Formato inválido";
-
         }
-
     }
-
-
 
     public function confirmacaoAgendamento(){
-
         $this->validaAutenticacao();
-
+      
         $response['post'] = $_POST;
-
         $paciente = Container::getModel('Consulta');
-
-
-
         $paciente->__set('medico_id',$_POST['medico_id']);
-
         $paciente->__set('dia',$_POST['dia']);
-
-
-
         $contaPac = $paciente->contaAtendidos();
-
-
-
     if($_POST['paciente'] != '' && $_POST['medico_id'] != '' && $_POST['prontuario'] != '' && $_POST['sus'] != '' && $_POST['horario'] != '' && $_POST['dia'] != ''){    
-
-        if($contaPac['dia'] < 16){
-
+        if($contaPac['dia'] < $_POST['atendimentos']){
             $response['mensagem'] = 'Gostaria de confirmar o agendamento?';
-
             $response['status'] = 0;
-
         }else{
-
             $response['mensagem'] = 'Numero de agendamentos dessa data superior ao permitido! Gostaria de inserir como Extra?';
-
             $response['status'] = 1;
-
         }
-
     }else{
-
         $response['mensagem'] = 'Não é possível realizar o agendamento, por favor preencha todos os campos';
-
         $response['status'] = 2;
-
     }
-
         echo json_encode($response, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
-
     }
-
-
 
     public function agendaPaciente(){
-
         $this->validaAutenticacao();
-
-        
-
         $dados_salvos = json_decode($_POST['save_data']);
-
-        /*echo '<pre>';
-
-        print_r($dados_salvos);
-
-        echo '</pre>';*/
-
-       
-
+        
         $paciente = Container::getModel('Consulta');
-
         $medicos = Container::getModel('Usuario');
-
-
-
             $paciente->__set('paciente',$dados_salvos->paciente);
-
             $paciente->__set('medico_id',$dados_salvos->medico_id);
-
             $paciente->__set('prontuario',$dados_salvos->prontuario);
-
             $paciente->__set('sus',$dados_salvos->sus);
-
             $paciente->__set('horario',$dados_salvos->horario);
-
             $paciente->__set('dia',$dados_salvos->dia);
-
             $paciente->__set('agendado_por',$dados_salvos->agendado_por);
-
             $paciente->agendaPaciente($_POST['status']);
-
-
-
         $response['mensagem'] = 'Paciente agendado com sucesso';
-
-
-
         echo json_encode($response, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
-
     }
-
-
-
-
 
     public function acao(){
-
         if(isset($_GET['action']) && $_GET['action'] == 'delete'){
-
             $usuario = Container::getModel('Usuario');
-
             $usuario->deletar($_GET['id']);
-
         }
-
-
-
         if(isset($_GET['action']) && $_GET['action'] == 'atender'){
-
             $consulta = Container::getModel('Consulta');
-
             $response = $consulta->atender($_GET['id']);
-
             $response['atendido'] = 's';
-
             echo json_encode($response, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
-
         }
-
+        if(isset($_GET['action']) && $_GET['action'] == 'atualizar'){
+            print_r($_GET);
+        }
     }
-
-
 
     public function filtraAgendamento(){
-
         $this-> validaAutenticacao();
-
-    
-
         $consulta = Container::getModel('Consulta');
-
         $medicos = Container::getModel('Usuario');
-
-
-
         $consulta->__set('paciente', $_POST['paciente']);
-
         $consulta->__set('prontuario', $_POST['prontuario']);
-
         $consulta->__set('horario', $_POST['horario']);
-
         $consulta->__set('dia', $_POST['data']);
-
         $consulta->__set('medico_id', $_POST['medico']);
-
-        
-
-        
-
         $response['filtro_array'] = $consulta->filtrar();
-
         echo json_encode($response, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
-
-
-
     }
 
-
-
-
-
-
-
     public function filtrarRelatorio(){
-
-
-
         $dates = FormInputFunctions::getDatesFromRange(date($_POST['data_inicio']), date($_POST['data_fim']));
-
         $atendidos = Container::getModel('Relatorio_Atendidos');
-
-
-
         //cria os arrays necessários
-
         $atendimentosPorDia = array();
-
         $medicos = array();
-
         $quantidadeAtendimentos = array();
-
         $post_medicos = array();
-
         $post_cores = array();
-
         $response = array();
-
         $responses = array();
-
         $cores = array();
-
-        
-
-
-
         //esse laço pega a variável dinâmica de médicos passadas do lado do frontEnd nomeada como medico_[i]
-
         foreach ($_POST as $k => $v) {
-
             if(strpos($k, 'medico_') === 0){
-
                 $post_medicos[$k]  = $v;
-
-                
-
             }
-
         }
-
-     
-
         foreach ($dates as $key => $date) {
-
             $atendidos->__set('dia', $date);
-
             foreach ($post_medicos as $med => $medico) {
-
                 $atendidos->__set('medico_id', $medico);
                 if(isset($_POST['flexRadioDefault']) && $_POST['flexRadioDefault'] == 'atendimentos'){
                     $atendimentosPorDia[$med][] = $atendidos->contagemAtendimentosPorDia()['realizados'];
@@ -465,81 +195,30 @@ class AppController extends Action{
                     $atendimentosPorDia[$med][] = $atendidos->contagemAgendamentosPorMedico()['atendidos'];
                 }
             }
-
         }
-
-
-
         foreach ($post_medicos as $keyM => $medico) {
-
-
-
             $usuario = Container::getModel('Usuario');
-
             $medicoNome = $usuario->selecionaMedicos($medico);
-
-
-
             $response['label'] = $medicoNome['nome'];
-
             $response['datas'] = $dates;
-
             $response['data'] = $atendimentosPorDia[$keyM];
-
-
-
             foreach ($dates as $key => $date) {
-
                 $atendidos->__set('dia', $date);
-
             }
-
-
-
                 $responses[] = $response;
-
         }
-
-
-
         foreach ($atendimentosPorDia as $key => $att) {
-
                 $response['atendimentos'] = $att;
-
         }
-
-
-
         echo json_encode($responses, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
-
     }
-
-
-
-    
 
     public function validaAutenticacao(){
-
-
-
         session_start();
-
-
-
         $this->view->usuario = $_SESSION['nome'];
-
-
-
         if(!isset($_SESSION['id']) || $_SESSION['id'] == '' || !isset($_SESSION['nome']) || $_SESSION['nome'] == ''){
-
             header('Location: /?login=erro2');
-
         }
-
     }
-
-
-
 }
-
 ?>
--- app_agendamento/App/Models/Usuario.php
+++ app_agendamento/App/Models/Usuario.php
@@ -1,102 +1,193 @@
-<?php
-namespace App\Models;
-
-use MF\Model\Model;
-
-class Usuario extends Model{
-
-    private $id;
-    private $nome;
-    private $email;
-    private $senha;
-    private $setor;
-
-    public function __get($attribute){
-        return $this->$attribute;
-    }
-
-    public function __set($attribute, $value){
-        $this->$attribute = $value;
-    }
-
-    public function autenticarUsuario(){
-        $query = "SELECT F.id, F.nome, S.setor, F.telefone, F.email FROM funcionarios AS F LEFT JOIN setores AS S ON F.id_setor = S.id Where F.email = :email AND F.senha = :senha";
-        $stmt = $this->db->prepare($query);
-        $stmt->bindValue(':email', $this->__get('email'));
-        $stmt->bindValue(':senha', $this->__get('senha'));
-        $stmt->execute();
-        $usuario = $stmt->fetch(\PDO::FETCH_ASSOC);
-
-        
-        if($usuario['id'] != '' && $usuario['nome'] != ''){
-            $this->__set('id', $usuario['id']);
-            $this->__set('nome', $usuario['nome']);
-            $this->__set('setor', $usuario['setor']);
-        }
-
-        return $this; 
-    }
-
-    public function cadastraFuncionario(){
-        $query = "INSERT INTO funcionarios (nome, id_setor, telefone, email, senha) VALUES (:nome, :id_setor, :telefone, :email, :senha)";
-        $stmt = $this->db->prepare($query);
-        $stmt->bindValue(':nome', $this->__get('nome'));
-        $stmt->bindValue(':id_setor', $this->__get('id_setor'));
-        $stmt->bindValue(':telefone', $this->__get('telefone'));
-        $stmt->bindValue(':email', $this->__get('email'));
-        $stmt->bindValue(':senha', md5($this->__get('senha')));
-        $stmt->execute();
-    }
-
-    public function listaFuncionarios(){
-        $query = 'SELECT F.id, F.nome, S.setor, F.telefone, F.email FROM funcionarios AS F LEFT JOIN setores AS S ON F.id_setor = S.id WHERE F.situacao = 1';
-        $stmt = $this->db->prepare($query);
-        $stmt->execute();
-        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
-    }
-
-    public function listaSetores(){
-        $query = "SELECT id, setor FROM setores";
-        $stmt = $this->db->prepare($query);
-        $stmt->execute();
-        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
-    }
-
-    public function listaMedicos(){
-        $query = "SELECT funcionarios.nome, funcionarios.id  FROM funcionarios WHERE id_setor = 2 AND funcionarios.situacao = 1";
-        $stmt = $this->db->prepare($query);
-        $stmt->execute();
-        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
-    }
-
-    public function listaMedicosAtivosInativos(){
-        $query = "SELECT funcionarios.nome, funcionarios.id  FROM funcionarios WHERE id_setor = 2";
-        $stmt = $this->db->prepare($query);
-        $stmt->execute();
-        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
-    }
-
-    public function selecionaMedicos($id){
-        $query = "SELECT funcionarios.nome FROM funcionarios WHERE id_setor = 2 AND id = :id";
-        $stmt = $this->db->prepare($query);
-        $stmt->bindValue(':id', $id);
-        $stmt->execute();
-        return $stmt->fetch(\PDO::FETCH_ASSOC);
-    }
-
-    public function listaAgendamentos(){
-        $query = "SELECT a.paciente, a.id, m.nome AS 'medico', a.prontuario, f.nome AS 'solicitante',a.atendimento_realizado, a.horario, DATE_FORMAT(a.dia, '%d/%m/%y') AS 'dia' FROM agendamentos AS a JOIN funcionarios AS m ON a.medico_id = m.id JOIN funcionarios AS f ON a.agendado_por = f.id";
-        $stmt = $this->db->prepare($query);
-        $stmt->execute();
-        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
-    }
-
-    public function deletar($id){
-        $query = "UPDATE funcionarios SET situacao = 0 WHERE funcionarios.id = :id;";
-        $stmt = $this->db->prepare($query);
-        $stmt->bindValue(':id', $id);
-        $stmt->execute();
-    }
-
-}
+<?php
+
+namespace App\Models;
+
+
+
+use MF\Model\Model;
+
+
+
+class Usuario extends Model{
+
+
+
+    private $id;
+
+    private $nome;
+
+    private $email;
+
+    private $senha;
+
+    private $setor;
+
+    private $id_horario;
+
+
+
+    public function __get($attribute){
+
+        return $this->$attribute;
+
+    }
+
+
+
+    public function __set($attribute, $value){
+
+        $this->$attribute = $value;
+
+    }
+
+
+
+    public function autenticarUsuario(){
+
+        $query = "SELECT F.id, F.nome, S.setor, F.telefone, F.email FROM funcionarios AS F LEFT JOIN setores AS S ON F.id_setor = S.id Where F.email = :email AND F.senha = :senha";
+
+        $stmt = $this->db->prepare($query);
+
+        $stmt->bindValue(':email', $this->__get('email'));
+
+        $stmt->bindValue(':senha', $this->__get('senha'));
+
+        $stmt->execute();
+
+        $usuario = $stmt->fetch(\PDO::FETCH_ASSOC);
+
+
+
+        
+
+        if($usuario['id'] != '' && $usuario['nome'] != ''){
+
+            $this->__set('id', $usuario['id']);
+
+            $this->__set('nome', $usuario['nome']);
+
+            $this->__set('setor', $usuario['setor']);
+
+        }
+
+
+
+        return $this; 
+
+    }
+
+
+
+    public function cadastraFuncionario(){
+
+        $query = "INSERT INTO funcionarios (nome, id_setor, telefone, email, senha) VALUES (:nome, :id_setor, :telefone, :email, :senha);
+        INSERT INTO horario_atendimentos(id_horario, atendimentos, id_funcionario) VALUES (:id_horario,12, LAST_INSERT_ID())";
+        $stmt = $this->db->prepare($query);
+        $stmt->bindValue(':nome', $this->__get('nome'));
+        $stmt->bindValue(':id_setor', $this->__get('id_setor'));
+        $stmt->bindValue(':id_horario', $this->__get('id_horario'));
+        $stmt->bindValue(':telefone', $this->__get('telefone'));
+        $stmt->bindValue(':email', $this->__get('email'));
+        $stmt->bindValue(':senha', md5($this->__get('senha')));
+        $stmt->execute();
+
+    }
+
+  
+    public function listaFuncionarios(){
+
+        $query = 'SELECT F.id, F.nome, S.setor, F.telefone, F.email, ha.atendimentos, TIME_FORMAT(h.horario, "%H:%i") AS horario FROM funcionarios AS F LEFT JOIN setores AS S ON F.id_setor = S.id LEFT JOIN horario_atendimentos AS ha ON ha.id_funcionario = F.id LEFT JOIN horarios AS h ON ha.id_horario = h.id WHERE F.situacao = 1';
+        $stmt = $this->db->prepare($query);
+        $stmt->execute();
+        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+    }
+
+
+    public function listaSetores(){
+        $query = "SELECT id, setor FROM setores";
+        $stmt = $this->db->prepare($query);
+        $stmt->execute();
+        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+    }
+
+    public function listaHorarios(){
+        $query = "SELECT id, TIME_FORMAT(horario, '%H:%i') AS horario FROM horarios";
+        $stmt = $this->db->prepare($query);
+        $stmt->execute();
+        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+    }
+
+
+
+    public function listaMedicos(){
+
+        $query = 'SELECT F.nome, F.id, ha.atendimentos,ha.id_horario, TIME_FORMAT(h.horario, "%H:%i") AS horario FROM funcionarios AS F LEFT JOIN setores AS S ON F.id_setor = S.id LEFT JOIN horario_atendimentos AS ha ON ha.id_funcionario = F.id LEFT JOIN horarios AS h ON ha.id_horario = h.id WHERE id_setor = 2 AND F.situacao = 1';
+        $stmt = $this->db->prepare($query);
+        $stmt->execute();
+        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+
+    }
+
+
+
+    public function listaMedicosAtivosInativos(){
+
+        $query = "SELECT funcionarios.nome, funcionarios.id  FROM funcionarios WHERE id_setor = 2";
+
+        $stmt = $this->db->prepare($query);
+
+        $stmt->execute();
+
+        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+
+    }
+
+
+
+    public function selecionaMedicos($id){
+
+        $query = "SELECT funcionarios.nome FROM funcionarios WHERE id_setor = 2 AND id = :id";
+
+        $stmt = $this->db->prepare($query);
+
+        $stmt->bindValue(':id', $id);
+
+        $stmt->execute();
+
+        return $stmt->fetch(\PDO::FETCH_ASSOC);
+
+    }
+
+
+
+    public function listaAgendamentos(){
+
+        $query = "SELECT a.paciente, a.id, m.nome AS 'medico', a.prontuario, f.nome AS 'solicitante',a.atendimento_realizado, a.horario, DATE_FORMAT(a.dia, '%d/%m/%y') AS 'dia' FROM agendamentos AS a JOIN funcionarios AS m ON a.medico_id = m.id JOIN funcionarios AS f ON a.agendado_por = f.id";
+
+        $stmt = $this->db->prepare($query);
+
+        $stmt->execute();
+
+        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+
+    }
+
+
+
+    public function deletar($id){
+
+        $query = "UPDATE funcionarios SET situacao = 0 WHERE funcionarios.id = :id;";
+
+        $stmt = $this->db->prepare($query);
+
+        $stmt->bindValue(':id', $id);
+
+        $stmt->execute();
+
+    }
+
+
+
+}
+
 ?>
