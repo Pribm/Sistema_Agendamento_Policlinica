cedc71efedd081e85c93b9a37532871d3e4a7c55
Paulo <monteiro.paulovinicius@gmail.com>
2021-07-27 17:23:32 -0300

    ajustes calendario

--- app_agendamento/public/js/ajustesCalendario.js
+++ app_agendamento/public/js/ajustesCalendario.js
@@ -6,9 +6,22 @@ $(document).ready(function () {
     $('#calendar').on('selectDate', function (event, newDate, oldDate) {
        let inputDay = document.getElementById('dia');
        inputDay.value = newDate
-       console.log(event.target)
+       dia = new Date(formatDate(inputDay.value))
+       console.log(dia.getDay())
     });
 
     $('#calendar').evoCalendar('selectDate', Date.now())
 })
+
+function formatDate(date){
+    let diaFormato = date
+
+    let ano = diaFormato.slice(0,4)
+    let dia = diaFormato.slice(6,8)
+    let mes = diaFormato.slice(4,6)
+        
+    diaFormato = ano + '-' + mes + '-' + dia + 'T00:00'
+
+    return diaFormato
+}
 
