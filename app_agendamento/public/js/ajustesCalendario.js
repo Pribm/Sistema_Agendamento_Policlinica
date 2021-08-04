31aa7fae92ee1bb8a2e717b7cab2b6795d4a7674
Paulo Monteiro <monteiro.paulovinicius@gmail.com>
2021-08-04 00:59:33 -0400

    funcionalidade desabilitar datas sem atendimentos

--- app_agendamento/public/js/ajustesCalendario.js
+++ app_agendamento/public/js/ajustesCalendario.js
@@ -19,15 +19,19 @@ $(document).ready(function () {
 
                    horarioSelecionado.dias.map(function(dia, index) {
                        if(dia_semana.value === dia.dia){
-
-                            //pega a contagem de atendimentos por dia do backend via método get
-                            $.get("contar_agendamentos", {dia:newDate, medico_id: medicoSelecionado}, function (response, textStatus, jqXHR) {
-                                let atendimentosTotais = (parseInt(dia.atendimentos) - parseInt(response[0]))
-                                $('#dias_disponiveis').append(`<p>${dia.label_dia}-feira, ${atendimentosTotais} atendimentos</p>`)
-
-                                //Aqui muda a cor do Alerta dos horários, caso os atendimentos totais sejam menores que 0
-                                atendimentosTotais <= 0 ? $('#alert-dias').addClass('alert-danger') : $('#alert-dias').removeClass('alert-danger');
-                            },"json");
+                            $.ajax({
+                                type: "get",
+                                url: "contar_agendamentos",
+                                data: {dia:newDate, medico_id: medicoSelecionado},
+                                dataType: "json",
+                                success: function (response) {
+                                    let atendimentosTotais = (parseInt(dia.atendimentos) - parseInt(response[0]))
+                                    $('#dias_disponiveis').html(`<p class='font-weight-bold text-uppercase text-success'>${dia.label_dia}-feira, ${atendimentosTotais} atendimentos</p>`)
+                                    if (atendimentosTotais <= 0){
+                                        $('#dias_disponiveis').html(`<p class='font-weight-bold text-uppercase text-danger'>${dia.label_dia}-feira, ${atendimentosTotais} atendimentos</p>`)
+                                    }
+                                }
+                            });
                        }
                    })
               }
